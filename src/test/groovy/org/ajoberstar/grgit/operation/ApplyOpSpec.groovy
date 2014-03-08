1636299c194c4d44f8841d6e304b4048278f236e
Andrew Oberstar <ajoberstar@gmail.com>
2014-03-08 12:41:08 -0600

    Changing ApplyOpSpec to use fixtures.

--- src/test/groovy/org/ajoberstar/grgit/operation/ApplyOpSpec.groovy
+++ src/test/groovy/org/ajoberstar/grgit/operation/ApplyOpSpec.groovy
@@ -15,10 +15,9 @@
  */
 package org.ajoberstar.grgit.operation
 
-import spock.lang.Specification
-
 import org.ajoberstar.grgit.Grgit
 import org.ajoberstar.grgit.Repository
+import org.ajoberstar.grgit.fixtures.SimpleGitOpSpec
 import org.ajoberstar.grgit.service.RepositoryService
 
 import org.eclipse.jgit.api.Git
@@ -26,17 +25,7 @@ import org.eclipse.jgit.api.Git
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
 
-class ApplyOpSpec extends Specification {
-	@Rule TemporaryFolder tempDir = new TemporaryFolder()
-
-	RepositoryService grgit
-
-	def setup() {
-		File repoDir = tempDir.newFolder('repo')
-		Git git = Git.init().setDirectory(repoDir).call()
-		grgit = Grgit.open(repoDir)
-	}
-
+class ApplyOpSpec extends SimpleGitOpSpec {
 	def 'apply with no patch fails'() {
 		when:
 		grgit.apply()
@@ -49,7 +38,7 @@ class ApplyOpSpec extends Specification {
 		repoFile('1.txt') << 'something'
 		repoFile('2.txt') << 'something else\n'
 		grgit.add(patterns:['.'])
-		grgit.repository.git.commit().setMessage('Test').call()
+		grgit.commit(message: 'Test')
 		def patch = tempDir.newFile()
 		this.class.getResourceAsStream('/org/ajoberstar/grgit/operation/sample.patch').withStream { stream ->
 			patch << stream
@@ -60,11 +49,5 @@ class ApplyOpSpec extends Specification {
 		repoFile('1.txt').text == 'something'
 		repoFile('2.txt').text == 'something else\nis being added\n'
 		repoFile('3.txt').text == 'some new stuff\n'
-	}
-
-	private File repoFile(String path, boolean makeDirs = true) {
-		def file = new File(grgit.repository.rootDir, path)
-		if (makeDirs) file.parentFile.mkdirs()
-		return file
 	}
 }
