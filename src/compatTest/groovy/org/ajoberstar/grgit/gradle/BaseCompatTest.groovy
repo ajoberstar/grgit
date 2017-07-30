62813502d2cf3749ae148986cd66d2fd80e37d61
Andrew Oberstar <ajoberstar@gmail.com>
2017-07-30 08:48:16 -0500

    Adding functional tests for org.ajoberstar.grgit
    Just basic testing that the Git repo is found or not.

---
+++ src/compatTest/groovy/org/ajoberstar/grgit/gradle/BaseCompatTest.groovy
@@ -0,0 +1,84 @@
+package org.ajoberstar.grgit.gradle
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import org.ajoberstar.grgit.Grgit
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+
+class BaseCompatTest extends Specification {
+  @Rule TemporaryFolder tempDir = new TemporaryFolder()
+  File projectDir
+  File buildFile
+
+  def setup() {
+    projectDir = tempDir.newFolder('project')
+    buildFile = projectFile('build.gradle')
+
+  }
+
+  def 'with no repo, plugin sets grgit to null'() {
+    given:
+    buildFile << '''\
+plugins {
+  id 'org.ajoberstar.grgit'
+}
+
+task doStuff {
+  doLast {
+    assert grgit == null
+  }
+}
+'''
+    when:
+    def result = build('doStuff')
+    then:
+    result.task(':doStuff').outcome == TaskOutcome.SUCCESS
+  }
+
+  def 'with repo, plugin opens the repo as grgit'() {
+    given:
+    Grgit git = Grgit.init(dir: projectDir)
+    projectFile('1.txt') << '1'
+    git.add(patterns: ['1.txt'])
+    git.commit(message: 'yay')
+    git.tag.add(name: '1.0.0')
+
+    buildFile << '''\
+plugins {
+  id 'org.ajoberstar.grgit'
+}
+
+task doStuff {
+  doLast {
+    println grgit.describe()
+  }
+}
+'''
+    when:
+    def result = build('doStuff', '--quiet')
+    then:
+    result.task(':doStuff').outcome == TaskOutcome.SUCCESS
+    result.output.normalize() == '1.0.0\n'
+  }
+
+  private BuildResult build(String... args) {
+    return GradleRunner.create()
+      .withGradleVersion(System.properties['compat.gradle.version'])
+      .withPluginClasspath()
+      .withProjectDir(projectDir)
+      .forwardOutput()
+      .withArguments((args + '--stacktrace') as String[])
+      .build()
+  }
+
+  private File projectFile(String path) {
+    File file = new File(projectDir, path)
+    file.parentFile.mkdirs()
+    return file
+  }
+}
