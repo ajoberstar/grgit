74d014eb991acfeff5cdebf641f3eff4249b92fb
Alex Saveau <saveau.alexandre@gmail.com>
2018-08-26 15:08:47 -0700

    Dynamically inject HasImplicitReceiver during canonicalization to circumvent compile-time safety
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- grgit-core/src/main/groovy/org/ajoberstar/grgit/Configurable.java
+++
@@ -1,9 +0,0 @@
-package org.ajoberstar.grgit;
-
-import org.gradle.api.HasImplicitReceiver;
-
-@FunctionalInterface
-@HasImplicitReceiver
-public interface Configurable<T> {
-  void configure(T t);
-}
