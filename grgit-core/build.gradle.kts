81906f8a2b4be913a22d775212f3c4a930c7f80b
Andrew Oberstar <andrew@ajoberstar.org>
2024-02-18 01:19:30 -0600

    Upgrade to Gradle 8
    Something changing in Gradle 8 or the Groovy compiler they use
    required me to separate the AST transformations into layers.
    
    Ultimately should be invisible from the outside (if I did this right).

--- grgit-core/build.gradle.kts
+++ grgit-core/build.gradle.kts
@@ -19,12 +19,18 @@ java {
   }
 }
 
-dependencies {
-  // groovy
-  api("org.codehaus.groovy:groovy:[3.0.9, 4.0)")
+val transform1 by sourceSets.creating
+val transform2 by sourceSets.creating
 
-  // jgit
+dependencies {
+  "transform1Implementation"("org.codehaus.groovy:groovy:[3.0.9, 4.0)")
+
+  "transform2Implementation"("org.codehaus.groovy:groovy:[3.0.9, 4.0)")
+  "transform2Implementation"(transform1.output)
+
+  api("org.codehaus.groovy:groovy:[3.0.9, 4.0)")
   api("org.eclipse.jgit:org.eclipse.jgit:[6.0, 7.0)")
+  api(transform2.output)
 }
 
 testing {
@@ -48,4 +54,6 @@ tasks.named<Jar>("jar") {
   manifest {
     attributes.put("Automatic-Module-Name", "org.ajoberstar.grgit")
   }
+  from(transform1.output)
+  from(transform2.output)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,12 @@
 pluginManagement {
   plugins {
-    id("org.ajoberstar.defaults.java-library") version "0.17.5"
-    id("org.ajoberstar.defaults.gradle-plugin") version "0.17.5"
+    id("org.ajoberstar.defaults.java-library") version "0.17.6"
+    id("org.ajoberstar.defaults.gradle-plugin") version "0.17.6"
 
-    id("org.ajoberstar.reckon.settings") version "0.18.0"
-    id("org.ajoberstar.stutter") version "0.7.2"
+    id("org.ajoberstar.reckon.settings") version "0.18.3"
+    id("org.ajoberstar.stutter") version "1.0.0"
 
-    id("com.diffplug.spotless") version "6.22.0"
+    id("com.diffplug.spotless") version "6.25.0"
   }
 
   repositories {
