0d321763b3009cac7cc3f28aa4e3b5868ee7a937
Andrew Oberstar <andrew@ajoberstar.org>
2025-05-06 22:11:33 -0500

    chore: Update plugin versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,12 @@
 pluginManagement {
   plugins {
-    id("org.ajoberstar.defaults.java-library") version "0.17.6"
-    id("org.ajoberstar.defaults.gradle-plugin") version "0.17.6"
+    id("org.ajoberstar.defaults.java-library") version "0.18.0"
+    id("org.ajoberstar.defaults.gradle-plugin") version "0.18.0"
 
-    id("org.ajoberstar.reckon.settings") version "0.18.3"
+    id("org.ajoberstar.reckon.settings") version "0.19.2"
     id("org.ajoberstar.stutter") version "1.0.0"
 
-    id("com.diffplug.spotless") version "6.25.0"
+    id("com.diffplug.spotless") version "7.0.3"
   }
 
   repositories {
